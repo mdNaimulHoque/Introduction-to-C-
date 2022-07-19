c502aefadb82531af600d3930f5e694921d66f2d
mdNaimulHoque <naimulhoq89@gmail.com>
2022-07-19 15:13:09 +0600

    float and double datatype print

---
+++ float_datatype.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    float a = 10.25789;
+    double b = 10.25715879;
+
+    cout << a << endl;
+    cout<<fixed<<setprecision(6)<<b<<endl;
+
+    return 0;
+}
